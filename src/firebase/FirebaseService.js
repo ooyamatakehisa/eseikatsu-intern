8890b3f8e912d35fb427a3aa8d615c04940bdc77
Satoshi Yamashita <satoshi.yamashita@e-seikatsu.co.jp>
2020-07-21 16:52:12 +0900

    Firebase用のクライアントを記述

---
+++ src/firebase/FirebaseService.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+
+export class FirebaseService {
+  static s_firebaseApp = firebase.initializeApp({
+    apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+    authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.VUE_APP_FIREBASE_APP_ID,
+  });
+
+  constructor() {
+    this.auth = FirebaseService.s_firebaseApp.auth();
+    this.database = FirebaseService.s_firebaseApp.database();
+  }
+}
