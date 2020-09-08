1771eaeb7edab9d3a825a02293e017549da9f5d5
ooyamatakehisa <takehisa.oyama110656@e-seikatsu.co.jp>
2020-09-08 13:49:03 +0900

    implement agent component

---
+++ src/components/Agent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    {{ agentData.agentShopName }}
+    TEL: {{ agentData.agentTelNumber }}
+    Email: {{ agentData.agentEmail }}
+    住所: {{ agentData.agentShopAddress}}
+  
+  </div>
+</template>
+
+<script lang="js">
+import { AgentQueryAPIApi } from "../dejima/dejima-client/src/index";
+
+export default {
+  name: "Agent",
+
+  components: {},
+
+  data: () => ({
+    agentData: {},
+  }),
+
+  methods: {
+    fetchAgentData() {
+      const apiClient = this.$store.state.apiServices.dejimaApiClient;
+      const AgentQueryAPIApi = new AgentQueryAPIApi(apiClient);
+      const propertyDetals = this.$store.getters.getProperty;
+      const customerKey = propertyDetals.customerKey;
+      this.agentData = await AgentQueryAPIApi.getAgentByCustomerKey(customerKey);
+    }
+  },
+
+  mounted() {
+    this.fetchAgentData();
+  } 
+}
+</script>
