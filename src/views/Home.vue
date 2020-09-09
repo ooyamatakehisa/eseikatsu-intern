1041a77b0683a5a144ac6c9ffc6f4b53560e386a
木口 優希 <yuki.kiguchi110654@e-seikatsu.co.jp>
2020-09-09 15:19:24 +0900

    refs issue #5 駅の検索

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,6 +6,12 @@
         <v-text-field v-model="priceUp" placeholder="例）50000（円以下）" />
         <v-btn v-on:click="onSearch()">検索</v-btn>
         <v-btn v-on:click="reset()">リセット</v-btn>
+
+        <label v-for="(station, index) in stationNameList" :key="index">
+          <input v-model="stationCode" :value="station.code" type="radio" />
+          {{ station.name }}
+        </label>
+
         <!-- <p>{{ queryResults }}</p> -->
         <div class="container" v-if="queryResults !== null">
           <bukken-property-card
@@ -16,8 +22,7 @@
         </div>
       </v-col>
     </v-row>
-      <v-btn @click="toImgAPI">imageAPI</v-btn>
-
+    <v-btn @click="toImgAPI">imageAPI</v-btn>
   </v-container>
 </template>
 
@@ -34,14 +39,18 @@ export default {
 
   data: () => ({
     queryResults: "",
+    queryStations: "",
     priceDown: "",
     priceUp: "",
+    stationCode: "",
+    stationNameList: "",
   }),
 
   methods: {
     reset: function() {
       this.priceDown = "";
       this.priceUp = "";
+      this.stationCode = "";
     },
     onSearch: async function(){
       const apiClient = this.$store.state.apiServices.dejimaApiClient;
@@ -49,14 +58,17 @@ export default {
       this.queryResults = await rentPropertyQueryAPIApi.searchRentPropertyByBuilding({
         priceFrom: this.priceDown,
         priceTo: this.priceUp,
+        stationCode: [this.stationCode]
       });
+
       this.saveSearchQuery(this.priceDown);
       this.saveSearchQuery(this.priceUp);
+      this.saveSearchQuery(this.stationCode);
     },
     saveSearchQuery: async function() {
       await this.$store.state.apiServices.firebaseService.database
         .ref("users/username")
-        .set({ query: [this.priceDown, this.priceUp] });
+        .set({ query: [this.priceDown, this.priceUp, this.stationCode] });
     },
     loadSearchQuery: async function() {
       const firebaseService = this.$store.state.apiServices.firebaseService;
@@ -66,15 +78,28 @@ export default {
       return result.val() ? result.val().query : "";
     },
   },
+
   mounted: async function(){
     const getQuery = await this.loadSearchQuery();
-    this.priceDown = getQuery[0];
-    this.priceUp = getQuery[1];
+    this.priceDown = getQuery[0]; // 家賃の下限
+    this.priceUp = getQuery[1];   // 家賃の上限
+    this.stationCode = getQuery[2]; // 駅コード
   },
 
-  // created() {
-  //   this.$store.commit("apiServices/initialize");
-  // }
+  created: async function() {
+    const apiClient = this.$store.state.apiServices.dejimaApiClient;
+    const rentPropertyQueryAPIApi = new RentPropertyQueryAPIApi(apiClient);
+    this.queryStations = await rentPropertyQueryAPIApi.aggregateRentPropertyByLine("station");
+
+    let values = [];
+    this.stationNameList = this.queryStations.results.filter(e => {
+      if (values.indexOf(e["name"]) === -1) {
+        // values に値が存在しない要素のみをフィルタリング
+        values.push(e["name"]);
+        return e;
+      }
+    });
+  }
 
 };
 </script>
